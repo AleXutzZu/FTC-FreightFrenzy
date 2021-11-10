979f32f7a0eb98927c2915190834fd85eaa452b4
AleXutzZu <alex.david@eminescusm.ro>
2021-11-10 20:06:37 +0200

    Update BasicMovement.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicMovement.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicMovement.java
@@ -1,18 +1,16 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
-import com.qualcomm.robotcore.util.Range;
 
 
 @TeleOp(name = "Basic: Movement", group = "Testing Purposes")
 public class BasicMovement extends OpMode {
     // Declare OpMode members.
-    private ElapsedTime runtime = new ElapsedTime();
+    private final ElapsedTime runtime = new ElapsedTime();
     private DcMotor rightFront;
     private DcMotor rightBack;
     private DcMotor leftFront;
