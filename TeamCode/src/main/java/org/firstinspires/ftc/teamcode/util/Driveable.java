44b432cb57f85a4a9516e42624f59356fb2f17c1
AleXutzZu <alex.david@eminescusm.ro>
2021-11-16 21:56:45 +0200

    Removed Driveable.java Successfully switched to abstract class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/Driveable.java
+++
@@ -1,7 +0,0 @@
-package org.firstinspires.ftc.teamcode.util;
-
-public interface Driveable {
-    void driveForward(float motorPower);
-    void driveBackward(float motorPower);
-    void resetMotors();
-}
