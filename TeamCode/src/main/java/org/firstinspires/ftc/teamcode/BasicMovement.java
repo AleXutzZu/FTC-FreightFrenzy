5f62c2fa113242d8fa3c8b1679ad600276eb1572
AleXutzZu <alex.david@eminescusm.ro>
2021-11-16 19:11:01 +0200

    Optimizations

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicMovement.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicMovement.java
@@ -5,8 +5,8 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-import org.firstinspires.ftc.teamcode.Util.Driveable;
-import org.firstinspires.ftc.teamcode.Util.RobotHardware;
+import org.firstinspires.ftc.teamcode.util.Driveable;
+import org.firstinspires.ftc.teamcode.hardware.RobotHardware;
 
 
 @TeleOp(name = "Basic: Movement", group = "Testing Purposes")
