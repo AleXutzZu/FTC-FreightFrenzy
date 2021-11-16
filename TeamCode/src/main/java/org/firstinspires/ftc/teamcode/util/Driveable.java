3a6320a8a0ea89e6ed4949f31f2e7457965d1809
AleXutzZu <alex.david@eminescusm.ro>
2021-11-16 11:38:14 +0200

    Update Driveable.java Added resetMotors() method to set the motors power to 0

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/Driveable.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/Driveable.java
@@ -3,4 +3,5 @@ package org.firstinspires.ftc.teamcode.util;
 public interface Driveable {
     void driveForward(float motorPower);
     void driveBackward(float motorPower);
+    void resetMotors();
 }
