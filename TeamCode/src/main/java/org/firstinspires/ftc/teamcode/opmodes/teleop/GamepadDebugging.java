0415c940d65828a965daeef30be94f52a549f2f7
AleXutzZu <alex.david@eminescusm.ro>
2021-11-17 21:30:51 +0200

    Added GamepadDebugging.java to ease buttons and joystick debugging while implementing the controls

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/GamepadDebugging.java
@@ -0,0 +1,30 @@
+package org.firstinspires.ftc.teamcode.opmodes.teleop;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name = "Gamepad Debugging", group = "Testing Purposes")
+public class GamepadDebugging extends OpMode {
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void init_loop() {
+    }
+
+
+    @Override
+    public void start() {
+    }
+
+    @Override
+    public void loop() {
+
+    }
+
+    @Override
+    public void stop() {
+    }
+}
