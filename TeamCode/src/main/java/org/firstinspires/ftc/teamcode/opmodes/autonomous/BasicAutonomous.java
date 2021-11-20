8a611066c8edada39e9d0f9224634984fdc7aa78
AleXutzZu <alex.david@eminescusm.ro>
2021-11-20 11:03:48 +0200

    Swapped class for LinearOpMode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autonomous/BasicAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autonomous/BasicAutonomous.java
@@ -1,37 +1,17 @@
 package org.firstinspires.ftc.teamcode.opmodes.autonomous;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 
 @Autonomous(name = "Basic Autonomous", group= "Testing Purposes")
-public class BasicAutonomous extends OpMode {
+public class BasicAutonomous extends LinearOpMode {
     private final ElapsedTime runtime = new ElapsedTime();
 
-    @Override
-    public void init() {
-        telemetry.addData("Status", "Initialized");
-    }
-
 
     @Override
-    public void init_loop() {
-    }
-
-
-    @Override
-    public void start() {
-        runtime.reset();
-    }
-
-    @Override
-    public void loop() {
-        telemetry.addData("Status", "Run Time: " + runtime.toString());
-    }
-
-    @Override
-    public void stop() {
+    public void runOpMode() throws InterruptedException {
 
     }
 }
