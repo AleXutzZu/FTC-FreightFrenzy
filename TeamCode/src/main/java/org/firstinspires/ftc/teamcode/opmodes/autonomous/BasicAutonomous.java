37fd4619a3ab8da37a284ac42e94794682cb9c63
AleXutzZu <alex.david@eminescusm.ro>
2021-11-20 12:45:55 +0200

    Update BasicAutonomous.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autonomous/BasicAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autonomous/BasicAutonomous.java
@@ -4,14 +4,32 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+import org.firstinspires.ftc.teamcode.control.Movements;
+
 
 @Autonomous(name = "Basic Autonomous", group= "Testing Purposes")
 public class BasicAutonomous extends LinearOpMode {
     private final ElapsedTime runtime = new ElapsedTime();
 
-
     @Override
     public void runOpMode() throws InterruptedException {
+        Movements robotMovements = new Movements(hardwareMap);
 
+        waitForStart();
+
+        robotMovements.driveForward(1f);
+        sleep(1000);
+        robotMovements.rotateLeft(1f);
+        sleep(500);
+        robotMovements.driveForward(1f);
+        sleep(1000);
+        robotMovements.rotateLeft(1f);
+        sleep(500);
+        robotMovements.driveForward(1f);
+        sleep(1000);
+        robotMovements.rotateLeft(1f);
+        sleep(500);
+        robotMovements.driveForward(1f);
+        sleep(1000);
     }
 }
