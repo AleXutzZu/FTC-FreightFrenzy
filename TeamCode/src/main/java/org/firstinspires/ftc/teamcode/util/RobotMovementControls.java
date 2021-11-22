68c50b3328674334139acfd729e671e29715f3d0
AleXutzZu <alex.david@eminescusm.ro>
2021-11-22 18:37:08 +0200

    Updated docs

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/RobotMovementControls.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/RobotMovementControls.java
@@ -27,7 +27,6 @@ public abstract class RobotMovementControls {
      * Drives the robot forwards by the specified power
      *
      * @param motorPower float value between 0.0 and 1.0 representing the power given to the motors (Less power means less speed)
-     * @see RobotMovementControls#driveForward(float, float)
      */
     public abstract void driveForward(float motorPower);
 
@@ -43,7 +42,6 @@ public abstract class RobotMovementControls {
      * Drives the robot backwards by the specified power
      *
      * @param motorPower float value between 0.0 and 1.0 representing the power given to the motors (Less power means less speed)
-     * @see RobotMovementControls#driveBackward(float, float)
      */
     public abstract void driveBackward(float motorPower);
 
@@ -59,7 +57,6 @@ public abstract class RobotMovementControls {
      * Drives the robot left by the specified power
      *
      * @param motorPower float value between 0.0 and 1.0 representing the power given to the motors (Less power means less speed)
-     * @see RobotMovementControls#driveLeft(float, float)
      */
     public abstract void driveLeft(float motorPower);
 
@@ -75,7 +72,6 @@ public abstract class RobotMovementControls {
      * Drives the robot right by the specified power
      *
      * @param motorPower float value between 0.0 and 1.0 representing the power given to the motors (Less power means less speed)
-     * @see RobotMovementControls#driveRight(float, float)
      */
     public abstract void driveRight(float motorPower);
 
