edb393e1fdbd9870bf0f788aab4abd55a8ec5ebf
AleXutzZu <alex.david@eminescusm.ro>
2021-11-26 21:39:58 +0200

    Updated docs

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/RobotHardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/RobotHardware.java
@@ -121,20 +121,6 @@ public class RobotHardware {
         wheelMotor.setPower(0f);
     }
 
-    /**
-     * Sets the corresponding run mode across all four motors
-     * @param runMode new run mode
-     * @see DcMotor.RunMode#RUN_TO_POSITION
-     * @see DcMotor.RunMode#STOP_AND_RESET_ENCODER
-     * @see DcMotor.RunMode#RUN_USING_ENCODER
-     */
-    public void setMotorModes(DcMotor.RunMode runMode){
-        rightFrontMotor.setMode(runMode);
-        rightBackMotor.setMode(runMode);
-        leftFrontMotor.setMode(runMode);
-        leftBackMotor.setMode(runMode);
-    }
-
     public DcMotor getRightFrontMotor() {
         return rightFrontMotor;
     }
