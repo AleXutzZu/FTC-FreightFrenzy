e385203d1652ddf43e5b10dc6c2b3fd24b413d30
AleXutzZu <alex.david@eminescusm.ro>
2021-11-26 13:03:29 +0200

    Update Movements.java Fixed encoders

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/control/Movements.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/control/Movements.java
@@ -79,6 +79,8 @@ public class Movements extends RobotMovementControls {
         robotHardware.rightBackMotor.setDirection(DcMotorSimple.Direction.REVERSE);
         robotHardware.leftBackMotor.setDirection(DcMotorSimple.Direction.FORWARD);
 
+        robotHardware.setMotorModes(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
         int drivingTarget = (int) (distance * TICKS_PER_CENTIMETRE);
 
         robotHardware.leftFrontMotor.setTargetPosition(drivingTarget);
@@ -129,6 +131,8 @@ public class Movements extends RobotMovementControls {
         robotHardware.rightBackMotor.setDirection(DcMotorSimple.Direction.REVERSE);
         robotHardware.leftBackMotor.setDirection(DcMotorSimple.Direction.REVERSE);
 
+        robotHardware.setMotorModes(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
         int drivingTarget = (int) (distance * TICKS_PER_CENTIMETRE);
 
         robotHardware.leftFrontMotor.setTargetPosition(drivingTarget);
@@ -178,6 +182,8 @@ public class Movements extends RobotMovementControls {
         robotHardware.leftFrontMotor.setDirection(DcMotorSimple.Direction.REVERSE);
         robotHardware.rightBackMotor.setDirection(DcMotorSimple.Direction.FORWARD);
         robotHardware.leftBackMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        robotHardware.setMotorModes(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
         int drivingTarget = (int) (distance * TICKS_PER_CENTIMETRE);
 
