56114f04d663b9659e2f918c93286359e11db21d
AleXutzZu <alex.david@eminescusm.ro>
2021-12-02 19:53:36 +0200

    Update RobotHardware.java Added better docs

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/RobotHardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/RobotHardware.java
@@ -2,26 +2,31 @@ package org.firstinspires.ftc.teamcode.hardware;
 
 import androidx.annotation.NonNull;
 
+import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 /**
+ * <h1>This class can be used to define all the specific hardware for the Perpetuum Mobile Robot.</h1>
  * This is NOT an OpMode.
- * <p>
- * This class can be used to define all the specific hardware for the Perpetuum Mobile Robot.
- *
- * Motors for driving the robot
- * Motor channel:  Right front motor:                                       "right_front"
- * Motor channel:  Right back motor:                                        "right_back"
- * Motor channel:  Left front motor:                                        "left_front"
- * Motor channel:  Left back motor:                                         "right_back"
- *
- * <p>Motor for using the elevator</p>
- * <p>Motor channel:  Elevator motor:                                          "elevator_motor"</p>
- *
- * <p>Motor to spin the carousel</p>
- * <p>Motor channel:  Wheel spinning motor                                     "wheel_motor"</p>
- *
+ * <br>
+ * <p>Used Motors (driving): <a href="https://www.pitsco.com/TETRIX-MAX-TorqueNADO-Motor-with-Encoder">TETRIX-MAX TorqueNADO Motor with Encoder</a></p>
+ * <p>Used Motors (lift and wheel): <a href="https://www.andymark.com/products/neverest-classic-40-gearmotor">NeveRest Classic 40 Gearmotor</a></p>
+ * <h2>Motors for driving the robot</h2>
+ * <pre>Right front motor:                                  <i>"right_front"</i></pre>
+ * <pre>Right back motor:                                   <i>"right_back"</i></pre>
+ * <pre>Left front motor:                                   <i>"left_front"</i></pre>
+ * <pre>Left back motor:                                    <i>"right_back"</i></pre>
+ * <br>
+ * <h2>Motor for using the elevator</h2>
+ * <pre>Elevator motor:                                 <i>"elevator_motor"</i></pre>
+ * <br>
+ * <h2>Motor to spin the carousel</h2>
+ * <pre>Wheel spinning motor:                               <i>"wheel_motor"</i></pre>
+ * <br>
+ * <h2>Servos</h2>
+ * <br>
+ * <h2>Sensors and misc</h2>
  */
 
 
@@ -29,12 +34,12 @@ public class RobotHardware {
     /*
     Motors
      */
-    private DcMotor rightFrontMotor = null;
-    private DcMotor rightBackMotor = null;
-    private DcMotor leftFrontMotor = null;
-    private DcMotor leftBackMotor = null;
+    private DcMotor rightFrontMotor = null;                  //right_front
+    private DcMotor rightBackMotor = null;                   //right_back
+    private DcMotor leftFrontMotor = null;                   //left_front
+    private DcMotor leftBackMotor = null;                    //left_back
     private DcMotor elevatorMotor = null;                    //elevator_motor
-    private DcMotor wheelMotor = null;
+    private DcMotor wheelMotor = null;                       //wheel_motor
 
     private static RobotHardware instance = null;
 
@@ -64,19 +69,36 @@ public class RobotHardware {
         rightBackMotor = hardwareMap.get(DcMotor.class, "right_back");
         leftBackMotor = hardwareMap.get(DcMotor.class, "left_back");
         leftFrontMotor = hardwareMap.get(DcMotor.class, "left_front");
-        elevatorMotor = hardwareMap.get(DcMotor.class, "elevator_motor");
-        wheelMotor = hardwareMap.get(DcMotor.class, "wheel_motor");
 
         rightFrontMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         rightBackMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         leftFrontMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         leftBackMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        /*
+        Defining the motor for the elevator
+         */
+        elevatorMotor = hardwareMap.get(DcMotor.class, "elevator_motor");
+
         elevatorMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        /*
+        Defining the motor to rotate the carousel
+        There is no need for an encoder
+         */
+        wheelMotor = hardwareMap.get(DcMotor.class, "wheel_motor");
+
         wheelMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        /*
+        Setting the power to 0f for starters
+         */
         rightFrontMotor.setPower(0f);
         rightBackMotor.setPower(0f);
         leftBackMotor.setPower(0f);
         leftFrontMotor.setPower(0f);
+        elevatorMotor.setPower(0f);
+        wheelMotor.setPower(0f);
     }
 
     public DcMotor getRightFrontMotor() {
