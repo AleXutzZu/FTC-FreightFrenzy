995713673ef9227ebcdac1303ec910ac8e94afe1
AleXutzZu <alex.david@eminescusm.ro>
2022-02-04 16:04:26 +0200

    Added OpModes for all possible spawn locations that park the robot in the storage unit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autonomous/RightSideFarPosition.java
@@ -0,0 +1,31 @@
+package org.firstinspires.ftc.teamcode.opmodes.autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.control.Limbs;
+import org.firstinspires.ftc.teamcode.control.Movements;
+import org.firstinspires.ftc.teamcode.hardware.RobotHardware;
+
+@Autonomous(name = "Demo: Right Side Far Position", group = "Demo Cluj-Napoca")
+public class RightSideFarPosition extends LinearOpMode {
+    private final ElapsedTime runtime = new ElapsedTime();
+    private final RobotHardware robotHardware = RobotHardware.getInstance();
+    private final Limbs robotLimbs = Limbs.getInstance();
+    private final Movements robotMovements = Movements.getInstance();
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        robotHardware.init(hardwareMap);
+        robotLimbs.useArm(true);
+        waitForStart();
+
+        if (opModeIsActive()) {
+            robotMovements.driveForward(1f, 32);
+            robotMovements.rotateRight(1f);
+            sleep(1000);
+            robotMovements.driveForward(1f, 230);
+        }
+    }
+}
