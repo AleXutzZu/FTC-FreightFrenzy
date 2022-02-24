f02d02f37b0fc8769e41e6d009ab1e2f5323c26f
AleXutzZu <alex.david@eminescusm.ro>
2022-02-24 12:42:29 +0200

    Implemented ProficientOmniMovement#useLimbs

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/movement/ProficientOmniMovement.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/movement/ProficientOmniMovement.java
@@ -1,11 +1,17 @@
 package org.firstinspires.ftc.teamcode.opmodes.teleop.movement;
 
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.teamcode.control.TeleOpControl;
 
 @TeleOp(name = "Proficient Omni Movement", group = "Testing Purposes")
 public class ProficientOmniMovement extends TeleOpControl {
+    /**
+     * Cooldown for opening/closing claws
+     */
+    private final ElapsedTime clawButtonCooldown = new ElapsedTime();
+
     @Override
     protected void drive() {
         double x = -gamepad1.left_stick_x;
@@ -43,6 +49,47 @@ public class ProficientOmniMovement extends TeleOpControl {
 
     @Override
     protected void useLimbs() {
+        /*
+        Key mappings
+        Right Trigger - Rotate wheel to the right
+        Left Trigger - Rotate wheel to the left
+        Right joystick - bring up/down the elevator
+        dpad up/down - put up/down the arm
+        LB - open/close claws
+         */
+        //Wheel rotation
+        float wheelPower = -gamepad2.right_trigger + gamepad2.left_trigger;
+        if (wheelPower != 0) {
+            useWheelMotor(wheelPower);
+            return;
+        } else useWheelMotor(0);
 
+        //Elevator controls
+        float elevatorY = -gamepad2.right_stick_y;
+        boolean isRightJoyStickActive = (elevatorY != 0);
+        if (isRightJoyStickActive) {
+            useElevator(elevatorY);
+            return;
+        } else useElevator(0);
+
+        //Arm controls
+        if (gamepad2.dpad_down || gamepad2.dpad_up) {
+            if (gamepad2.dpad_up && !gamepad2.dpad_down) {
+                useArm(true);
+                return;
+            }
+            if (!gamepad2.dpad_up) {
+                useArm(false);
+                return;
+            }
+        }
+
+        //Claw control
+        if (gamepad2.b) {
+            if (clawButtonCooldown.time() >= 0.5) {
+                clawButtonCooldown.reset();
+                useClaws();
+            }
+        }
     }
 }
