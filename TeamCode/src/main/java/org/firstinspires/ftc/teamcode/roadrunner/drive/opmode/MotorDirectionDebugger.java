fcda265e2b2b65a95206974451cc7cb0c667e3d3
AleXutzZu <alex.david@eminescusm.ro>
2022-02-28 22:18:56 +0200

    Modified motor direction debugger to show encoder ticks

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/drive/opmode/MotorDirectionDebugger.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/drive/opmode/MotorDirectionDebugger.java
@@ -5,36 +5,36 @@ import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
-import org.firstinspires.ftc.teamcode.roadrunner.drive.SampleMecanumDrive;
 
 /**
  * This is a simple teleop routine for debugging your motor configuration.
  * Pressing each of the buttons will power its respective motor.
- *
+ * <p>
  * Button Mappings:
- *
+ * <p>
  * Xbox/PS4 Button - Motor
- *   X / ▢         - Front Left
- *   Y / Δ         - Front Right
- *   B / O         - Rear  Right
- *   A / X         - Rear  Left
- *                                    The buttons are mapped to match the wheels spatially if you
- *                                    were to rotate the gamepad 45deg°. x/square is the front left
- *                    ________        and each button corresponds to the wheel as you go clockwise
- *                   / ______ \
- *     ------------.-'   _  '-..+              Front of Bot
- *              /   _  ( Y )  _  \                  ^
- *             |  ( X )  _  ( B ) |     Front Left   \    Front Right
- *        ___  '.      ( A )     /|       Wheel       \      Wheel
- *      .'    '.    '-._____.-'  .'       (x/▢)        \     (Y/Δ)
- *     |       |                 |                      \
- *      '.___.' '.               |          Rear Left    \   Rear Right
- *               '.             /             Wheel       \    Wheel
- *                \.          .'              (A/X)        \   (B/O)
- *                  \________/
- *
+ * X / ▢         - Front Left
+ * Y / Δ         - Front Right
+ * B / O         - Rear  Right
+ * A / X         - Rear  Left
+ * The buttons are mapped to match the wheels spatially if you
+ * were to rotate the gamepad 45deg°. x/square is the front left
+ * ________        and each button corresponds to the wheel as you go clockwise
+ * / ______ \
+ * ------------.-'   _  '-..+              Front of Bot
+ * /   _  ( Y )  _  \                  ^
+ * |  ( X )  _  ( B ) |     Front Left   \    Front Right
+ * ___  '.      ( A )     /|       Wheel       \      Wheel
+ * .'    '.    '-._____.-'  .'       (x/▢)        \     (Y/Δ)
+ * |       |                 |                      \
+ * '.___.' '.               |          Rear Left    \   Rear Right
+ * '.             /             Wheel       \    Wheel
+ * \.          .'              (A/X)        \   (B/O)
+ * \________/
+ * <p>
  * Uncomment the @Disabled tag below to use this opmode.
  */
 @Config
@@ -46,7 +46,15 @@ public class MotorDirectionDebugger extends LinearOpMode {
     public void runOpMode() throws InterruptedException {
         telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
 
-        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+        DcMotor leftFront = hardwareMap.get(DcMotor.class, "left_front");
+        DcMotor leftBack = hardwareMap.get(DcMotor.class, "left_back");
+        DcMotor rightFront = hardwareMap.get(DcMotor.class, "right_front");
+        DcMotor rightBack = hardwareMap.get(DcMotor.class, "right_back");
+
+        leftBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        leftFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         telemetry.addLine("Press play to begin the debugging opmode");
         telemetry.update();
@@ -68,20 +76,23 @@ public class MotorDirectionDebugger extends LinearOpMode {
             telemetry.addLine("<font face=\"monospace\">&nbsp;&nbsp;A / X&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;- Rear&nbsp;&nbsp;Left</font>");
             telemetry.addLine();
 
-            if(gamepad1.x) {
-                drive.setMotorPowers(MOTOR_POWER, 0, 0, 0);
-                telemetry.addLine("Running Motor: Front Left");
-            } else if(gamepad1.y) {
-                drive.setMotorPowers(0, 0, 0, MOTOR_POWER);
-                telemetry.addLine("Running Motor: Front Right");
-            } else if(gamepad1.b) {
-                drive.setMotorPowers(0, 0, MOTOR_POWER, 0);
-                telemetry.addLine("Running Motor: Rear Right");
-            } else if(gamepad1.a) {
-                drive.setMotorPowers(0, MOTOR_POWER, 0, 0);
-                telemetry.addLine("Running Motor: Rear Left");
+            if (gamepad1.x) {
+                leftFront.setPower(MOTOR_POWER);
+                telemetry.addLine("Running Motor: Front Left pos: " + leftFront.getCurrentPosition());
+            } else if (gamepad1.y) {
+                rightFront.setPower(MOTOR_POWER);
+                telemetry.addLine("Running Motor: Front Right pos: " + rightFront.getCurrentPosition());
+            } else if (gamepad1.b) {
+                rightBack.setPower(MOTOR_POWER);
+                telemetry.addLine("Running Motor: Rear Right pos: " + rightBack.getCurrentPosition());
+            } else if (gamepad1.a) {
+                leftBack.setPower(MOTOR_POWER);
+                telemetry.addLine("Running Motor: Rear Left pos: " + leftBack.getCurrentPosition());
             } else {
-                drive.setMotorPowers(0, 0, 0, 0);
+                leftBack.setPower(0);
+                leftFront.setPower(0);
+                rightBack.setPower(0);
+                rightFront.setPower(0);
                 telemetry.addLine("Running Motor: None");
             }
 
